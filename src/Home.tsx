2d4d99dee9e2e5a20c903e00b88d27c13ac057ee
Daniela Vornic <daniela.vornic@gmail.com>
2022-11-17 14:16:17 +0200

    Update Home page with nickname form

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,52 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { redirect, useNavigate } from 'react-router-dom';
+import { useUser } from './hooks/useUser';
+
 const Home = () => {
-  return <div>Home</div>;
+  const { setNickname } = useUser();
+  const navigate = useNavigate();
+  const [localNickname, setLocalNickname] = useState('');
+
+  const handleNicknameChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setLocalNickname(e.target.value);
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // TODO: validate localNickname before setting it
+    setNickname(localNickname);
+    navigate('/chat');
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-lg -mt-16">
+          <img
+            src="/images/home-bird.svg"
+            alt="bird"
+            className="w-1/2 mb-6 ml-auto mr-auto"
+          />
+          <h1 className="text-3xl font-bold mb-12">Chat App</h1>
+          <form id="nickname-form" onSubmit={handleFormSubmit}>
+            <input
+              className="input input-md w-64"
+              name="nickname"
+              placeholder="Nickname"
+              onChange={handleNicknameChange}
+            />
+            <button
+              form="nickname-form"
+              className="btn btn-primary btn-md ml-4"
+              type="submit"
+            >
+              Get Started
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

6435e2c13c5d241f29bb0df027f95b95a7f87d43
Daniela Vornic <daniela.vornic@gmail.com>
2022-11-17 14:15:46 +0200

    Add ts interface for User context

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,9 +1,17 @@
 import { createContext, useState } from 'react';
 
-export const UserContext = createContext({});
+interface UserContextProps {
+  nickname: string;
+  setNickname: (nickname: string) => void;
+}
+
+export const UserContext = createContext<UserContextProps>({
+  nickname: '',
+  setNickname: () => {},
+});
 
 export const UserProvider = ({ children }: any) => {
-  const [nickname, setNickname] = useState({});
+  const [nickname, setNickname] = useState('');
 
   return (
     <UserContext.Provider value={{ nickname, setNickname }}>
